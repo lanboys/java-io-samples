404e8933460c53443ce2668791ded87716954921
lanboys <lan_bing2013@163.com>
2020-08-05 14:59:12 +0800

    first commit

---
+++ src/com/bing/lan/socket/nio/MultiplexerTimeServer.java
@@ -0,0 +1,156 @@
+package com.bing.lan.socket.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by lb on 2020/8/4.
+ * https://blog.csdn.net/Heron22/article/details/106132064
+ */
+
+public class MultiplexerTimeServer implements Runnable {
+
+    private Selector selector;
+    private ServerSocketChannel serverSocketChannel;
+    private volatile boolean stop = false;
+    private int count = 0;
+
+    public synchronized void addCount() {
+        count++;
+        System.out.println("===================================================addCount(): " + count);
+    }
+
+    public static void main(String[] args) {
+        MultiplexerTimeServer server = new MultiplexerTimeServer(8088);
+        new Thread(server, "timerServer-001").start();
+    }
+
+    /**
+     * @Desc 初始化多路复用器，绑定端口
+     */
+    public MultiplexerTimeServer(int port) {
+        try {
+            //创建socket 通道
+            serverSocketChannel = ServerSocketChannel.open();
+            //设置通道为非阻塞
+            serverSocketChannel.configureBlocking(false);
+            //绑定ip端口，设置最大的请求连接数为1024
+            serverSocketChannel.socket().bind(new InetSocketAddress(port), 2048);
+
+            //创建Reactor线程多路复用器
+            selector = Selector.open();
+            //注册到多路复用器上，监听accept事件
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("*********************************************");
+            System.out.println("****  time server is start on port " + port + "  ****");
+            System.out.println("*********************************************");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                System.out.println("\n\nrun(): " + Thread.currentThread().getName() + " " + System.currentTimeMillis() + " loop...");
+                //设置获取就绪key的休眠时间,每间隔1秒唤醒一次
+                selector.select(10000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                SelectionKey key;
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+                    iterator.remove();
+                    try {
+                        handlerKey(key);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        key.cancel();
+                        if (key.channel() != null)
+                            key.channel().close();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handlerKey(SelectionKey key) throws IOException {
+        System.out.println("----------handlerKey(): " + Thread.currentThread().getName() + key.hashCode());
+        if (!key.isValid()) {
+            System.out.println("----------handlerKey(): key无效");
+            return;
+        }
+        //查看是否是accept事件
+        if (key.isAcceptable()) {
+            System.out.println("----------handlerKey(): accept事件");
+            ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+            //接受客户端请求，三次握手结束，建立物理连接
+            SocketChannel accept = channel.accept();
+            //设置非阻塞模式
+            accept.configureBlocking(false);
+            //注册监听读取事件
+            accept.register(selector, SelectionKey.OP_READ);
+            return;
+        }
+
+        //判断是否可读状态
+        if (!key.isReadable()) {
+            return;
+        }
+
+        //查看是read事件
+        System.out.println("----------handlerKey(): read事件");
+        //开辟缓存空间
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        //读取数据
+        int readBytes = socketChannel.read(readBuffer);
+        //大于0，读取到了数据
+        if (readBytes > 0) {
+            readBuffer.flip();
+            byte[] bytes = new byte[readBuffer.remaining()];
+            readBuffer.get(bytes);
+            String msg = new String(bytes, "UTF-8");
+            System.out.println("----------handlerKey(): 读取到了数据");
+            System.out.println("--------------------<<<<<< receive msg: " + msg);
+            doWrite(socketChannel, System.currentTimeMillis() + "");
+        } else if (readBytes < 0) {
+            //等于-1 ，链路已经关闭，需要释放资源
+            System.out.println("----------handlerKey(): 链路已经关闭");
+            key.cancel();
+            socketChannel.close();
+        } else {
+            //等于0，没有可读取数据，忽略
+            System.out.println("----------handlerKey(): 没有可读取数据");
+        }
+    }
+
+    private void doWrite(SocketChannel socketChannel, String response) throws IOException {
+        byte[] bytes = response.getBytes();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length);
+        byteBuffer.put(bytes);
+        byteBuffer.flip();
+        socketChannel.write(byteBuffer);
+        System.out.println("-------------------->>>>>> response msg: " + response);
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+}
---
+++ src/com/bing/lan/socket/nio/TimeClientHandler.java
@@ -0,0 +1,147 @@
+package com.bing.lan.socket.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by lb on 2020/8/4.
+ */
+public class TimeClientHandler {
+
+    private Selector selector;
+    private volatile boolean stop = false;
+    private int count = 0;
+
+    public synchronized void addCount() {
+        count++;
+        System.out.println("addCount(): " + count);
+    }
+
+    public static void main(String[] args) {
+        TimeClientHandler timeClientHandler = new TimeClientHandler();
+        Selector selector = timeClientHandler.getSelector();
+        for (int i = 0; i < 2000; i++) {
+            //new Thread(new Runnable() {
+            //    @Override
+            //    public void run() {
+            try {
+                SocketChannel socketChannel = SocketChannel.open();
+                socketChannel.configureBlocking(false);
+                socketChannel.connect(new InetSocketAddress("127.0.0.1", 8088));
+                //注册到多路复用器上，监听连接事件
+                socketChannel.register(selector, SelectionKey.OP_CONNECT);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+            //    }
+            //}).start();
+        }
+        timeClientHandler.start();
+    }
+
+    public TimeClientHandler() {
+        try {
+            selector = Selector.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Selector getSelector() {
+        return selector;
+    }
+
+    public void start() {
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                SelectionKey key;
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+                    iterator.remove();
+                    try {
+                        handInput(key);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        key.cancel();
+                        if (key.channel() != null) {
+                            key.channel().close();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handInput(SelectionKey key) throws IOException {
+        if (!key.isValid()) {
+            return;
+        }
+        SocketChannel channel = (SocketChannel) key.channel();
+        //判断是否连接成功
+        if (key.isConnectable()) {
+            //完成了连接
+            if (channel.finishConnect()) {
+                channel.register(selector, SelectionKey.OP_READ);
+                addCount();
+                doWrite(channel);
+            } else {
+                System.exit(-1);
+            }
+        }
+
+        //判断是否可读状态
+        if (!key.isReadable()) {
+            return;
+        }
+
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        int readBytes = channel.read(readBuffer);
+        if (readBytes > 0) {
+            readBuffer.flip();
+            byte[] bytes = new byte[readBuffer.remaining()];
+            readBuffer.get(bytes);
+            String msg = new String(bytes, "UTF-8");
+            System.out.println("<<<<<< client receive msg: " + msg);
+            channel.close();
+        } else if (readBytes < 0) {
+            key.cancel();
+            channel.close();
+        }
+    }
+
+    private void stop() {
+        this.stop = true;
+    }
+
+    private void doWrite(SocketChannel socketChannel) throws IOException {
+        byte[] bytes = "query for time".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+        writeBuffer.put(bytes);
+        writeBuffer.flip();
+        socketChannel.write(writeBuffer);
+        if (!writeBuffer.hasRemaining()) {
+            System.out.println(">>>>>> client send msg success");
+        }
+    }
+}
