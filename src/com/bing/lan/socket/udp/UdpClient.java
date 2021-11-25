52bbfa187ace40fbf47e2905df5c99454dc91f3c
lanboys <lan_bing2013@163.com>
2021-11-25 10:36:11 +0800

    更新udp客户端

--- src/com/bing/lan/socket/udp/UdpClient.java
+++ src/com/bing/lan/socket/udp/UdpClient.java
@@ -13,10 +13,15 @@ public class UdpClient {
 
   public static void main(String[] argv) throws IOException {
     StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < 65507; i++) {//加上协议头32，最多 65539=65507+32
+    // 本机之间的 MTU 65536, ip协议是使用两个字节( 65535 byte )来存储长度
+    // 本机最多 65539 - 4 = 65535(ip包最大长度) = 20 + 8 + 65507 ;  20为ip头，8为 udp头
+    for (int i = 0; i < 65507; i++) {
       builder.append("a");
     }
-    byte[] buf = builder.toString().getBytes();
+
+    String msg = builder.toString();
+    System.out.println("length : " + msg.length());
+    byte[] buf = msg.getBytes();
     DatagramSocket socket = new DatagramSocket();
     InetAddress address = InetAddress.getByName("localhost");
     DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 8088);
